d266cf7cbf717828695ba68c39de33f9c4d58722
kiang <kiange@gmail.com>
2016-07-14 11:34:46 +0800

    add router to have sharable link

--- map/main.js
+++ map/main.js
@@ -1,5 +1,87 @@
 $.ajaxSetup({async: false});
 
+var showCunli = function (theYear, theButton, cunliCode) {
+    if (!cunliCode) {
+        cunliCode = '';
+    }
+    var valueKeys = {
+        playButton1: 'avg',
+        playButton2: 'mid',
+        playButton3: 'sd',
+        playButton4: 'mid1',
+        playButton5: 'mid3'
+    };
+    console.log([theYear, theButton, cunliCode]);
+    currentYear = theYear;
+    currentButton = theButton;
+
+    $('a.btn-year').each(function () {
+        if ($(this).attr('data-year') === currentYear) {
+            $(this).removeClass('btn-default').addClass('btn-primary');
+        } else {
+            $(this).removeClass('btn-primary').addClass('btn-default');
+        }
+    });
+    $('.btn-primary').removeClass('active disabled').find('.glyphicon').hide();
+    $('#' + currentButton).addClass('active disabled').find('.glyphicon').show();
+    cunli.forEach(function (value) {
+        var key = value.getProperty('VILLAGE_ID'),
+                count = 0;
+        if (cunliSalary[key] && cunliSalary[key][currentYear]) {
+            count = cunliSalary[key][currentYear][valueKeys[theButton]];
+        }
+        value.setProperty('num', count);
+        if (cunliCode === key) {
+            showFeature(value);
+        }
+    });
+
+    map.data.setStyle(function (feature) {
+        color = ColorBar(feature.getProperty('num'));
+        return {
+            fillColor: color,
+            fillOpacity: 0.6,
+            strokeColor: 'gray',
+            strokeWeight: 1
+        }
+    });
+};
+
+function showFeature(feature) {
+    var cunli = feature.getProperty('C_Name') + feature.getProperty('T_Name') + feature.getProperty('V_Name');
+    var cunliKey = feature.getProperty('VILLAGE_ID');
+    var headerPrinted = false;
+    var detail = '<h3>' + cunli + '</h3><div style="float:right;">單位：金額(千元)</div><table class="table table-boarded">';
+    var targetHash = '#' + currentYear + '/' + currentButton + '/' + cunliKey;
+    var foundFeatureGeo = feature.getGeometry().getAt(0).getArray();
+    if (cunliSalary[cunliKey]) {
+        for (y in cunliSalary[cunliKey]) {
+            var yLine = '<tr><td>' + y + '</td>';
+            for (k in cunliSalary[cunliKey][y]) {
+                if (false === headerPrinted) {
+                    detail += '<tr><td>年度</td><td>納稅單位</td><td>綜合所得總額</td><td>平均數</td><td>中位數</td><td>第一分位數</td><td>第三分位數</td><td>標準差</td><td>變異係數</td></tr>';
+                    headerPrinted = true;
+                }
+                yLine += '<td>' + cunliSalary[cunliKey][y][k] + '</td>';
+            }
+            detail += yLine + '</tr>';
+        }
+    }
+    detail += '</table>';
+    $('#cunliDetail').html(detail);
+    if (window.location.hash !== targetHash) {
+        window.location.hash = targetHash;
+    }
+    map.setCenter(foundFeatureGeo[0]);
+    map.setZoom(15);
+}
+
+var routes = {
+    '/:theYear/:theButton/:cunliCode': showCunli,
+    '/:theYear/:theButton': showCunli
+};
+var router = Router(routes);
+
 var map, currentYear = '2014', currentButton = 'playButton2',
         currentPlayIndex = false,
         cunli, cunliSalary;
@@ -8,7 +90,6 @@ $.getJSON('fia_data.json', function (data) {
     cunliSalary = data;
 });
 function initialize() {
-
     /*map setting*/
     $('#map-canvas').height(window.outerHeight / 2.2);
 
@@ -20,7 +101,7 @@ function initialize() {
     $.getJSON('cunli.json', function (data) {
         cunli = map.data.addGeoJson(topojson.feature(data, data.objects.cunli));
     });
-
+    router.init();
 
     map.data.addListener('mouseover', function (event) {
         var Cunli = event.feature.getProperty('C_Name') + event.feature.getProperty('T_Name') + event.feature.getProperty('V_Name');
@@ -30,25 +111,7 @@ function initialize() {
     });
 
     map.data.addListener('click', function (event) {
-        var cunli = event.feature.getProperty('C_Name') + event.feature.getProperty('T_Name') + event.feature.getProperty('V_Name');
-        var cunliKey = event.feature.getProperty('VILLAGE_ID');
-        var headerPrinted = false;
-        var detail = '<h3>' + cunli + '</h3><div style="float:right;">單位：金額(千元)</div><table class="table table-boarded">';
-        if (cunliSalary[cunliKey]) {
-            for (y in cunliSalary[cunliKey]) {
-                var yLine = '<tr><td>' + y + '</td>';
-                for (k in cunliSalary[cunliKey][y]) {
-                    if (false === headerPrinted) {
-                        detail += '<tr><td>年度</td><td>納稅單位</td><td>綜合所得總額</td><td>平均數</td><td>中位數</td><td>第一分位數</td><td>第三分位數</td><td>標準差</td><td>變異係數</td></tr>';
-                        headerPrinted = true;
-                    }
-                    yLine += '<td>' + cunliSalary[cunliKey][y][k] + '</td>';
-                }
-                detail += yLine + '</tr>';
-            }
-        }
-        detail += '</table>';
-        $('#cunliDetail').html(detail);
+        showFeature(event.feature);
     });
 
     map.data.addListener('mouseout', function (event) {
@@ -58,142 +121,42 @@ function initialize() {
 
     $('#playButton1').on('click', function () {
         currentButton = 'playButton1';
-        $('.btn-primary').removeClass('active disabled').find('.glyphicon').hide();
-        $(this).addClass('active disabled').find('.glyphicon').show();
-        cunli.forEach(function (value) {
-            var key = value.getProperty('VILLAGE_ID'),
-                    count = 0;
-            if (cunliSalary[key] && cunliSalary[key][currentYear]) {
-                count = cunliSalary[key][currentYear]['avg'];
-            }
-            value.setProperty('num', count);
-        });
-
-        map.data.setStyle(function (feature) {
-            color = ColorBar(feature.getProperty('num'));
-            return {
-                fillColor: color,
-                fillOpacity: 0.6,
-                strokeColor: 'gray',
-                strokeWeight: 1
-            }
-        });
+        window.location.hash = '#' + currentYear + '/' + currentButton;
         return false;
     });
 
     $('#playButton2').on('click', function () {
         currentButton = 'playButton2';
-        $('.btn-primary').removeClass('active disabled').find('.glyphicon').hide();
-        $(this).addClass('active disabled').find('.glyphicon').show();
-        cunli.forEach(function (value) {
-            var key = value.getProperty('VILLAGE_ID'),
-                    count = 0;
-            if (cunliSalary[key] && cunliSalary[key][currentYear]) {
-                count = cunliSalary[key][currentYear]['mid'];
-            }
-            value.setProperty('num', count);
-        });
-
-        map.data.setStyle(function (feature) {
-            color = ColorBar(feature.getProperty('num'));
-            return {
-                fillColor: color,
-                fillOpacity: 0.6,
-                strokeColor: 'gray',
-                strokeWeight: 1
-            }
-        });
+        window.location.hash = '#' + currentYear + '/' + currentButton;
         return false;
     });
 
     $('#playButton3').on('click', function () {
         currentButton = 'playButton3';
-        $('.btn-primary').removeClass('active disabled').find('.glyphicon').hide();
-        $(this).addClass('active disabled').find('.glyphicon').show();
-        cunli.forEach(function (value) {
-            var key = value.getProperty('VILLAGE_ID'),
-                    count = 0;
-            if (cunliSalary[key] && cunliSalary[key][currentYear]) {
-                count = cunliSalary[key][currentYear]['sd'];
-            }
-            value.setProperty('num', count);
-        });
-
-        map.data.setStyle(function (feature) {
-            color = ColorBar(feature.getProperty('num'));
-            return {
-                fillColor: color,
-                fillOpacity: 0.6,
-                strokeColor: 'gray',
-                strokeWeight: 1
-            }
-        });
+        window.location.hash = '#' + currentYear + '/' + currentButton;
         return false;
     });
 
     $('#playButton4').on('click', function () {
         currentButton = 'playButton4';
-        $('.btn-primary').removeClass('active disabled').find('.glyphicon').hide();
-        $(this).addClass('active disabled').find('.glyphicon').show();
-        cunli.forEach(function (value) {
-            var key = value.getProperty('VILLAGE_ID'),
-                    count = 0;
-            if (cunliSalary[key] && cunliSalary[key][currentYear]) {
-                count = cunliSalary[key][currentYear]['mid1'];
-            }
-            value.setProperty('num', count);
-        });
-
-        map.data.setStyle(function (feature) {
-            color = ColorBar(feature.getProperty('num'));
-            return {
-                fillColor: color,
-                fillOpacity: 0.6,
-                strokeColor: 'gray',
-                strokeWeight: 1
-            }
-        });
+        window.location.hash = '#' + currentYear + '/' + currentButton;
         return false;
     });
 
     $('#playButton5').on('click', function () {
         currentButton = 'playButton5';
-        $('.btn-primary').removeClass('active disabled').find('.glyphicon').hide();
-        $(this).addClass('active disabled').find('.glyphicon').show();
-        cunli.forEach(function (value) {
-            var key = value.getProperty('VILLAGE_ID'),
-                    count = 0;
-            if (cunliSalary[key] && cunliSalary[key][currentYear]) {
-                count = cunliSalary[key][currentYear]['mid3'];
-            }
-            value.setProperty('num', count);
-        });
-
-        map.data.setStyle(function (feature) {
-            color = ColorBar(feature.getProperty('num'));
-            return {
-                fillColor: color,
-                fillOpacity: 0.6,
-                strokeColor: 'gray',
-                strokeWeight: 1
-            }
-        });
+        window.location.hash = '#' + currentYear + '/' + currentButton;
         return false;
     });
 
-    $('#' + currentButton).trigger('click');
-
     $('a.btn-year').click(function () {
         currentYear = $(this).attr('data-year');
-        $('a.btn-year').each(function () {
-            if ($(this).attr('data-year') === currentYear) {
-                $(this).removeClass('btn-default').addClass('btn-primary');
-            } else {
-                $(this).removeClass('btn-primary').addClass('btn-default');
-            }
-        });
-        $('#' + currentButton).trigger('click');
+        window.location.hash = '#' + currentYear + '/' + currentButton;
+        return false;
     });
+    if (window.location.hash == '' || window.location.hash == '#') {
+        window.location.hash = '#' + currentYear + '/' + currentButton;
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
