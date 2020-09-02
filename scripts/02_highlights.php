2c8d7eb71d679e9c5a2b4d9cf26251b8386f080e
kiang <kiange@gmail.com>
2020-09-03 02:19:44 +0800

    update highlights to use 2018 data

--- scripts/02_highlights.php
+++ scripts/02_highlights.php
@@ -1,26 +1,26 @@
 <?php
 $basePath = dirname(__DIR__);
-$cunli = json_decode(file_get_contents($basePath . '/map/cunli.json'), true);
+$cunli = json_decode(file_get_contents($basePath . '/map/20180330.json'), true);
 $ref = array();
-foreach($cunli['objects']['20150401']['geometries'] AS $obj) {
-  $ref[$obj['properties']['VILLAGE_ID']] = $obj['properties'];
+foreach($cunli['objects']['20180330']['geometries'] AS $obj) {
+  $ref[$obj['properties']['VILLCODE']] = $obj['properties'];
 }
 $json = json_decode(file_get_contents($basePath . '/map/fia_data.json'), true);
 $stack = array();
 foreach($json AS $cunliCode => $lv1) {
   foreach($lv1 AS $year => $lv2) {
-    if($year === 2016) {
+    if($year === 2018) {
       if(!isset($stack[$lv2['mid']])) {
         $stack[$lv2['mid']] = array();
       }
-      $stack[$lv2['mid']][$cunliCode] = isset($ref[$cunliCode]) ? $ref[$cunliCode]['C_Name'] . $ref[$cunliCode]['T_Name'] . $ref[$cunliCode]['V_Name'] : '';
+      $stack[$lv2['mid']][$cunliCode] = isset($ref[$cunliCode]) ? $ref[$cunliCode]['COUNTYNAME'] . $ref[$cunliCode]['TOWNNAME'] . $ref[$cunliCode]['VILLNAME'] : '';
     }
   }
 }
 
-ksort($stack);
+krsort($stack);
 foreach($stack AS $income => $lv1) {
   foreach($lv1 AS $cunliCode => $cunliName) {
-    echo "{$income},{$cunliName},https://goo.gl/nz3N1T#2016/mid/{$cunliCode}\n";
+    echo "{$income},{$cunliName},https://goo.gl/nz3N1T#2018/mid/{$cunliCode}\n";
   }
 }
