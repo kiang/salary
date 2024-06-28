363d233da8811ff6f327698539c1f86a60723295
kiang <kiange@gmail.com>
2024-06-28 13:42:09 +0800

    update json and sort cunli keys

--- scripts/01_fia_raw.php
+++ scripts/01_fia_raw.php
@@ -268,4 +268,5 @@ foreach($years AS $year) {
     }
 }
 
+ksort($result);
 file_put_contents($basePath . '/docs/map/fia_data.json', json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
